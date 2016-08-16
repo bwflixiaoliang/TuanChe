3310add2a543f809e591548ad88a6243b3dd34cd
lixiaoliang1988 <327480918@qq.com>
2016-08-16 16:25:15 +0800

    first time

---
+++ app/src/main/java/com/bwf/tuanche/MainActivity.java
@@ -0,0 +1,33 @@
+package com.bwf.tuanche;
+
+import android.view.View;
+
+import com.bwf.framwork.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public int getContentViewId() {
+        return 0;
+    }
+
+    @Override
+    public void beforeInitView() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
